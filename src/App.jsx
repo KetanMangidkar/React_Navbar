0d8871de96aa1fb73bf8020b86628591663bea0c
KetanMangidkar <ketanmangidkar@gmail.com>
2022-04-23 00:58:36 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+     <div class="navbar"> 
+         <div><h2>LOGOBAKERY</h2></div>
+         <div class='names'>
+           <div>Search</div>
+           <div>Project</div>
+           <div>About</div> 
+         </div>
+         <div><button class='button'>Contact</button></div>
+     </div>
+  );
+}
+
+export default App;
